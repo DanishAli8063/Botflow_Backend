fd9c7bf603946321ea0f0f6b55e35ef22f805467
Danish Ali <123619499+DanishAli8063@users.noreply.github.com>
2024-06-11 20:35:46 +0500

    Delete fastapi_botflow directory

--- fastapi_botflow/utils/utils.py
+++
@@ -1,8 +0,0 @@
-def lower_and_replace_spaces(input_string):
-    # Convert the input string to lowercase
-    lowercased_string = input_string.lower()
-
-    # Replace spaces with underscores
-    replaced_string = lowercased_string.replace(' ', '_')
-
-    return replaced_string
